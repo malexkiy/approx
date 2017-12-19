4953edec8aa25411879b0466ef846904e85ebac2
malexkiy <malexkiy@gmail.com>
2017-12-20 00:18:03 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -2,6 +2,21 @@
 #include <vector>
 #include <catch.hpp>
 
+bool veccmp(std::vector<double> a, std::vector<double> b)
+{
+    if (a.size() != b.size())
+	    return false;
+	
+    for (size_t i = 0; i < a.size(); i++)
+    {
+        if (a[i] != b[i])
+	{
+            return false;
+        }
+    }
+	
+    return true;
+}
 SCENARIO("approx test", "[test]") {
 	std::vector<double>
 		X = { 0, 0.1, 0.2, 0.3, 0.5, 0.7, 0.9 },
@@ -12,9 +27,9 @@ SCENARIO("approx test", "[test]") {
 		_apY = { 2.9999999998453211, 4.5000000010598793, 1.6999999978024625, 0.70000000162359388, -1.0000000004023093, -1.9999999999242846, 3.9999999999945430 };
 
 	coefs = nma::polyfit(X, Y, 6);
-	REQUIRE(_coefs == coefs);
+	REQUIRE(veccmp(_coefs, coefs));
 
 	apY = nma::polyval(coefs, X);
-	REQUIRE(_apY == apY);
+	REQUIRE(veccmp(_apY, apY));
 
 }
